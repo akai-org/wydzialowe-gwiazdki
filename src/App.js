edeb92e403cf78ae69effe85aa472159709d0612
Magda <magda.skurczynska@wp.pl>
2018-10-15 18:47:24 +0200

    change

--- src/App.js
+++ src/App.js
@@ -2,24 +2,52 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class App extends Component   {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1>Wydzialowe gwiazdki</h1>
-          <ComponentOne />
+          <Header title="abc"/>
+          <ComponentTwo />
         </header>
+
       </div>
     );
   }
 }
 
-const ComponentOne = () => {
+/*const ComponentOne = ( props   ) => {
   return (
-    <p>component 1</p>
+    <div>
+    
+    <h1>Tytuł {props.name}</h1>
+    
+    </div>
+  )
+}*/
+const ComponentTwo = () =>{
+  return (
+    <div>
+      <h1>Tekst 1</h1>
+      <h2>Test 2</h2>
+    </div>
   )
 }
 
+class Header extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      currentDate: "15.10.2018"}
+  }
+  render() {
+    return (
+      <h1>{this.state.currentDate }</h1>
+    //<ComponentOne name="tytul"/>
+    )
+   }
+}
 export default App;
+
