fe1e3071f43ce3acfd51a2a208b8eadaf0234c34
Damian <damiankarbowiak17@wp.pl>
2018-10-16 20:03:22 +0200

    Dodanie TailContainer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TailContainer from './TailContainer.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1>Wydzialowe gwiazdki</h1>
-          <ComponentOne />
+          {/*<TailContainer />*/}
         </header>
       </div>
     );
   }
 }
 
-const ComponentOne = () => {
-  return (
-    <p>component 1</p>
-  )
-}
 
 export default App;
---
+++ src/TailContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './TailContainer.css';
+
+const TailContainer = () => {
+    return(
+        <div className="TailContainer">
+            <h1>Halo</h1>
+        </div>
+        
+    )
+}
+
+export default TailContainer;
