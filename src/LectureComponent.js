585fb5c45289a429e1deed030bcc5347b7f8b9de
Damian <damiankarbowiak17@wp.pl>
2018-10-17 21:40:11 +0200

    Edit LectureComponent

--- src/LectureComponent.js
+++ src/LectureComponent.js
@@ -1,19 +1,10 @@
 import React, { Component } from 'react';
 import './LectureComponent.css';
-import StarsComponent from './StarsComponent.js'
 
 const LectureComponent = () => {
     return(
-        <div className="LectureComponent">
+        <div className="LectureComponent">  
         
-            <div id="Stars">
-                <StarsComponent />
-                <StarsComponent />
-                <StarsComponent />
-                <StarsComponent />
-                <StarsComponent />
-                <StarsComponent />
-            </div>  
         </div>
     )
 }
