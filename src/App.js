50cd295cceff5c3d6b8c9843eaf23e0eaf11cbde
Magda <magda.skurczynska@wp.pl>
2018-10-18 19:37:47 +0200

    Person

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header'; 
+import Person from './components/Person'; 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
       <Header title='Wydziałowe Gwiazdki'></Header>
+      <Person name="Pan" surname="XYZ" function="Doktor"></Person>
       <Footer></Footer>  
       </div>
     );
