80b6ca5f78444f5807ecc239a54f83506a98a0e5
Damian <damiankarbowiak17@wp.pl>
2018-10-18 19:11:17 +0200

    Edit StarsComponent

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import TailContainer from './TailContainer.js';
-import LectureComponent from './LectureComponent.js';
 
 class App extends Component {
   render() {
