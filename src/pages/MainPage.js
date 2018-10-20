1e6f77f285f814755d535caeb2af52e20edc4a0d
Mateusz Stempniewicz <mateusz.stempniewicz@gmail.com>
2018-10-20 17:21:51 +0200

    remove TailContainer from MainPage

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,13 +1,10 @@
 import React from 'react';
 
-import TailContainer from './../containers/TailContainer';
-
 class MainPage extends React.Component {
   render() {
     return (
       <div>
         <h2>Main Page</h2>
-        <TailContainer />
       </div>
     )
   }
