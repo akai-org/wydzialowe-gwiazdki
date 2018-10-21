36e5edec6a1842343d4c43861942331781995721
Magda <magda.skurczynska@wp.pl>
2018-10-21 17:48:29 +0200

    new header, new footer, logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,10 @@
 import React  from 'react';
 const Footer = (props)=>{
     return(
-      <p class="footerClass">Projekt stworzony przez uczestników Koła Akai</p>
+      <div class="footerClass">
+        <img src={require('./logo2.png')} alt="logo" class="logoFooter"/>
+        <p >Projekt stworzony przez uczestników Koła Akai</p>
+      </div>
     )
   
   }
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 const Header = (pros) => {
   return(
-    <div>
-      <h1 class="header">{pros.title}</h1>
+    <div class="header">
+      <img src={require('./logo2.png')} alt="logo" class="logo"/>
+      <h1>{pros.title}</h1>  
     </div>
   )
 }
