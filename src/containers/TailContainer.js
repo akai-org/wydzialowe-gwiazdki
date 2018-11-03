0be00a7439dd4493c1f67daf479edd264a3f0ca0
Mateusz Stempniewicz <mateusz.stempniewicz@gmail.com>
2018-11-03 21:56:24 +0100

    new header view

--- src/containers/TailContainer.js
+++ src/containers/TailContainer.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
+import Select from 'react-select';
 import './TailContainer.scss';
 import Tail from './../components/Tail';
 import lessons from './../resources/lessons';
-import { SubjectSubscriber } from 'rxjs/internal/Subject';
 
 const convertTails = () => lessons.map(lesson =>{return (<Tail title={lesson.name}/>)});
 
@@ -17,22 +17,26 @@ class TailContainer extends Component {
       handleChange(event) {
         this.setState({value: event.target.value});
       }
+
+    selectOptions = [
+        { value: 1, label: 'Semestr 1' },
+        { value: 2, label: 'Semestr 2' },
+        { value: 3, label: 'Semestr 3' },
+        { value: 4, label: 'Semestr 4' },
+        { value: 5, label: 'Semestr 5' },
+        { value: 6, label: 'Semestr 6' },
+        { value: 7, label: 'Semestr 7' }
+    ];
     
     render() {
         return(
             <div>
                 <form  className="styled-select">
-                    <label>
-                        <select value={this.state.value} onChange={this.handleChange}>
-                            <option value="Semestr 1">Semestr 1</option>
-                            <option value="Semestr 2">Semestr 2</option>
-                            <option value="Semestr 3">Semestr 3</option>
-                            <option value="Semestr 4">Semestr 4</option>
-                            <option value="Semestr 5">Semestr 5</option>
-                            <option value="Semestr 6">Semestr 6</option>
-                            <option value="Semestr 7">Semestr 7</option>
-                        </select>
-                    </label>
+                    <Select
+                        options={this.selectOptions}
+                        name="semester"
+                        defaultValue={this.selectOptions[0]}
+                    />
                 </form>
                 <div className="TailContainer">     
                     {convertTails()}
