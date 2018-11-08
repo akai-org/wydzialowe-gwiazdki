e62104f31a3d25ce0c73b987979544e5dc47ef31
Mateusz Stempniewicz <mateusz.stempniewicz@gmail.com>
2018-11-08 18:08:17 +0100

    change select

--- src/containers/TailContainer.js
+++ src/containers/TailContainer.js
@@ -4,45 +4,45 @@ import './TailContainer.scss';
 import Tail from './../components/Tail';
 import lessons from './../resources/lessons';
 
-const convertTails = () => lessons.map(lesson =>{return (<Tail title={lesson.name}/>)});
-
 class TailContainer extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {value: 'Semestr 1'};
+  constructor(props) {
+    super(props);
+    this.state = {
+      semesterSelectValue: 1
+    };
+  }
     
-        this.handleChange = this.handleChange.bind(this);
-      }
-    
-      handleChange(event) {
-        this.setState({value: event.target.value});
-      }
+  handleChange = option => this.setState({semesterSelectValue: option.value});
+  convertTails = () => lessons.filter(lesson => lesson.semester == this.state.semesterSelectValue)
+    .map(lesson => <Tail title={lesson.name}/> );
 
-    selectOptions = [
-        { value: 1, label: 'Semestr 1' },
-        { value: 2, label: 'Semestr 2' },
-        { value: 3, label: 'Semestr 3' },
-        { value: 4, label: 'Semestr 4' },
-        { value: 5, label: 'Semestr 5' },
-        { value: 6, label: 'Semestr 6' },
-        { value: 7, label: 'Semestr 7' }
-    ];
+  selectOptions = [
+    { value: 1, label: 'Semestr 1' },
+    { value: 2, label: 'Semestr 2' },
+    { value: 3, label: 'Semestr 3' },
+    { value: 4, label: 'Semestr 4' },
+    { value: 5, label: 'Semestr 5' },
+    { value: 6, label: 'Semestr 6' },
+    { value: 7, label: 'Semestr 7' }
+  ];
     
-    render() {
-        return(
-            <div>
-                <form  className="styled-select">
-                    <Select
-                        options={this.selectOptions}
-                        name="semester"
-                        defaultValue={this.selectOptions[0]}
-                    />
-                </form>
-                <div className="TailContainer">     
-                    {convertTails()}
-                </div>
-            </div>
-    )}
+  render() {
+    return(
+      <div>
+        <form  className="styled-select">
+          <Select
+            options={this.selectOptions}
+            name="semester"
+            defaultValue={this.selectOptions[0]}
+            onChange={this.handleChange}
+          />
+        </form>
+        <div className="TailContainer">     
+          {this.convertTails()}
+        </div>
+      </div>
+    )
+  }
 }
 
 export default TailContainer;
