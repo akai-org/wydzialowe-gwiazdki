ccdaafb9be6853b3a83d5d6a422176e2dda7f585
Mateusz Stempniewicz <mateusz.stempniewicz@gmail.com>
2018-11-27 19:54:35 +0100

    add Loader

--- src/containers/LayoutContainer/index.js
+++ src/containers/LayoutContainer/index.js
@@ -1,20 +1,24 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import './index.scss';
 
+import Loader from '../../components/Loader';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
-import TailContainer from '../TailContainer';
 import LectureContainer from '../LectureContainer';
+
+const TailContainer = lazy(() => import('../TailContainer'));
 
 const LayoutContainer = () => (
   <div className="LayoutContainer">
     <Header title="WydziałoweGwizadki" />
     <Router>
       <Switch>
-        <Route exact path="/" component={TailContainer} />
-        <Route path="/lecture/:id" component={LectureContainer} />
+        <Suspense fallback={<Loader />}>
+          <Route exact path="/" render={() => <TailContainer />} />
+          <Route path="/lecture/:id" component={LectureContainer} />
+        </Suspense>
       </Switch>
     </Router>
     <Footer />
