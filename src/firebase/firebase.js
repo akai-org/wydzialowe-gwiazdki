5186bef667c668c1a0bd224bf1c528e93178efbd
Mateusz Stempniewicz <mateusz.stempniewicz@gmail.com>
2018-11-29 14:19:35 +0100

    add env file to firebase connection

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 const databaseConfig = {
-  apiKey: 'AIzaSyBcpzCPJttC1vOlq_Khm203662wg61cTz8',
-  authDomain: 'wydzialowe-gwiazdki.firebaseapp.com',
-  databaseURL: 'https://wydzialowe-gwiazdki.firebaseio.com',
-  projectId: 'wydzialowe-gwiazdki',
-  storageBucket: '',
-  messagingSenderId: '5779775193'
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(databaseConfig);
