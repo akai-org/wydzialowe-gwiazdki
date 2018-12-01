661194dafbfc553b9c902a9d9b529fe77a3473c6
Biniox <Jarek>
2018-12-01 08:46:20 +0100

    sd

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+// import * from 'firebase';
+
+//  var config = {
+//     apiKey: "AIzaSyAUHC-EWcKjEbEA1BXdhO536RI6OrPEqKA",
+//     authDomain: "lecturerating-1.firebaseapp.com",
+//     databaseURL: "https://lecturerating-1.firebaseio.com",
+//     // projectId: "lecturerating-1",
+//     // messagingSenderId: "991660156927",
+//     storageBucket: "lecturerating-1.appspot.com"
+//   };
+//   firebase.initializeApp(config);
+
+//   const db = firebase.database();
+
+//   export default db;
