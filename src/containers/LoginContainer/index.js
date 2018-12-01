e26fc289241d45c15ef8f4cb48c2b7c5d9c7e1d7
Jarek B <biniox2@gmail.com>
2018-12-01 21:41:12 +0100

    Add token

--- src/containers/LoginContainer/index.js
+++ src/containers/LoginContainer/index.js
@@ -1,49 +1,110 @@
 import React, { Component } from 'react';
-import * as firebase from 'firebase';
-// import db from "../../firebase";
+import { firebase } from '../../firebase';
 import './index.scss';
 
+// function write() {
+//   firebase
+//     .database()
+//     .ref('users/').push()
+//     .set({
 
+//       name: 'MaciekS',
+//       surname: 'Dejniak'
 
+//     });
+// }
+// write();
 
-// Add additional services that you want to use
-require("firebase/auth");
-require("firebase/database");
-require("firebase/messaging");
-require("firebase/functions");
+// firebase.database().ref("tokens/dwadaw/uid").on("value", (snapshot) => {
+//   console.log(snapshot.val());
 
-
-function write() {
-  firebase
-    .database()
-    .ref('users/1')
-    .set({
-      name: 'ddd',
-      surname: 'www'
-    });
-  console.log('dziaba');
-}
-write();
+//   (snapshot.val() === null) ? console.log("tak") : console.log("nie");
+// });
 
 class LoginContainer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      showCommunicate: false
+    };
+  }
+
+  isSign = authCookie => {
+    let a = null;
+    firebase
+      .database()
+      .ref(`tokens/${authCookie}/uid`)
+      .on('value', snapshot => {
+        // console.log(snapshot.val());
+        // console.log(authCookie);
+        console.log(snapshot.val());
+        a = snapshot.val();
+        if (a === null) {
+          a = false;
+        } else {
+          a = true;
+        }
+      });
+    return a;
+  };
+
+  signIn = () => {
+    const name = document.getElementById('name').value;
+    // console.log(name);
+    const pass = document.getElementById('pass').value;
+    // console.log(pass);
+
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(name, pass)
+      .then(() => {
+        firebase.auth().onAuthStateChanged(user => {
+          if (user) {
+            // const token = user.getIdToken()
+            // console.log(token[1]);
+            const date = new Date();
+            console.log(user.refreshToken);
+            firebase
+              .database()
+              .ref(`tokens/${user.refreshToken}`)
+              .set({
+                uid: user.uid,
+                date: date.toTimeString()
+              });
+
+            date.setTime(date.getTime() + 10 * 1000);
+            document.cookie = `token=${user.refreshToken}; expires=10000`;
+          }
+        });
+      })
+      .catch(() => {
+        this.setState({
+          showCommunicate: true
+        });
+      });
+  };
+
   render() {
+    const cookie = document.cookie.split(/; */)[0].split('=');
+    console.log(this.isSign(cookie[1]));
     return (
       <div className="LoginContainer">
-        <form method="POST" action="dsdw.js">
-          <label>Login:</label>
-          <br />
-          <input type="text" className="name" name="login" />
-          <br />
-          <label>Hasło:</label>
-          <br />
-          <input type="password" className="pass" name="password" />
-          <br />
-          <input type="checkbox" value="remember" /> Zapamiętaj mnie
-          <br />
-          <input type="submit" className="submit" name="submit" value="Login" />
-          <br />
-          <a href="/register">Nie masz jeszcze konta ?</a>
-        </form>
+        <div className="error">
+          {this.state.showCommunicate ? 'Nie poprawny email lub hasło' : ''}
+        </div>
+        <label>Login:</label>
+        <br />
+        <input type="text" value="nazwa@nazwa.pl" id="name" className="name" name="login" />
+        <br />
+        <label>Hasło:</label>
+        <br />
+        <input type="password" value="Haslo12" id="pass" className="pass" name="password" />
+        <br />
+        <input type="checkbox" value="remember" /> Zapamiętaj mnie
+        <br />
+        <input onClick={this.signIn} type="submit" className="submit" name="submit" value="Login" />
+        <br />
+        <a href="/register">Nie masz jeszcze konta ?</a>
       </div>
     );
   }
--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,15 @@
 import * as firebase from 'firebase';
 
 const config = {
-  apiKey: "AIzaSyAUHC-EWcKjEbEA1BXdhO536RI6OrPEqKA",
-  authDomain: "lecturerating-1.firebaseapp.com",
-  databaseURL: "https://lecturerating-1.firebaseio.com",
-  projectId: "lecturerating-1",
-  messagingSenderId: "991660156927",
-  storageBucket: "lecturerating-1.appspot.com"
+  apiKey: 'AIzaSyAUHC-EWcKjEbEA1BXdhO536RI6OrPEqKA',
+  authDomain: 'lecturerating-1.firebaseapp.com',
+  databaseURL: 'https://lecturerating-1.firebaseio.com',
+  projectId: 'lecturerating-1',
+  messagingSenderId: '991660156927',
+  storageBucket: 'lecturerating-1.appspot.com'
 };
 firebase.initializeApp(config);
 
 const db = firebase.database();
 
-export default db;
+export { db, firebase };
